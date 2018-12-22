46ffb037019cf3adbd61c250f6fec93f7dd0266d
Dave Brophy <dave@brophy.uk>
2018-12-22 20:43:47 +0000

    Static files

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ func main() {
 	page := NewPage()
 
 	vecty.SetTitle("Whisper Simulation")
-	vecty.AddStylesheet("css/bulma.css")
-	vecty.AddStylesheet("css/bulma-extensions.min.css")
-	vecty.AddStylesheet("css/custom.css")
+	vecty.AddStylesheet("https://cdn.jsdelivr.net/gh/divan/whispervis@c395bc39dd76ba9d9118693da3d7bc90f234e5f9/css/bulma.css")
+	vecty.AddStylesheet("https://cdn.jsdelivr.net/gh/divan/whispervis@c395bc39dd76ba9d9118693da3d7bc90f234e5f9/css/bulma-extensions.min.css")
+	vecty.AddStylesheet("https://cdn.jsdelivr.net/gh/divan/whispervis@c395bc39dd76ba9d9118693da3d7bc90f234e5f9/css/custom.css")
 	vecty.RenderBody(page)
 }
